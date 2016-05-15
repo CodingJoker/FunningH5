6ce6a53cfbb8cad7e858cc39d7f39101982b0af1
CodingJoker <550928460@qq.com>
2016-05-15 11:57:17 +0800

    dir init

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+// 静态服务器
+var options = {
+    fe:{
+        src:'FE'
+    }
+}
+gulp.task('server',['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir:options.fe.src,
+        },
+        ui: {
+            port: 8080
+        },
+        open: "external",
+        startPath:"/tpls/share_bad_friends.html"
+    });
+    gulp.watch([options.fe.src + "/tpls/*.html"]).on('change',browserSync.reload);
+    gulp.watch([options.fe.src + "/js/*.js"]).on('change',browserSync.reload);
+    gulp.watch(options.fe.src + "/scss.scss",['sass']);
+    gulp.watch([options.fe.src + "/third_party/**"]).on('change',browserSync.reload);
+});
+gulp.task('sass', function() {
+    return gulp.src(options.fe.src + "/scss/*.scss")
+        .pipe(sass())
+        .on('error', function(err) {
+         console.log('Sass Error!', err.message);
+         this.emit('end');
+        })
+        .pipe(gulp.dest(options.fe.src + "/css"))
+        .pipe(browserSync.reload({stream: true}));
+});
+
