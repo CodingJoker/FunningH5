d386fa0909defe09efc7d26700bdec7b0fecf4a2
CodingJoker <550928460@qq.com>
2016-06-03 11:22:00 +0800

    测试chrome通知

--- FE/js/share_bad_friends.js
+++ FE/js/share_bad_friends.js
@@ -1,4 +1,28 @@
-$("body").on('tap','.more-icon-down',function(){
-	$(this).siblings('.js-comment-text').removeClass('more');
-	console.log('touch')
-})
+
+var Car = function(wheel,type,speed){
+	this.wheel = wheel || 0;
+	this.type = type || 0;
+	this.speed = speed || '50km/s';
+	return this;
+}
+
+Car.prototype = {
+	constructor:Car,
+	getSpeed : function(){
+		console.log("This Car's speed is "+this.speed);
+		return this;
+	},
+	getWheel : function(){
+		console.log("This Car Has " + this.wheel + " Wheels");
+		return this;
+	}
+
+}
+Car.prototype.getType  = function(){
+		console.log("This Car's Type is " + this.type);
+		return this;
+}
+
+var car_1 = new Car();
+car_1.getSpeed().getWheel();
+console.log(Car.prototype.isPrototypeOf(car_1))
